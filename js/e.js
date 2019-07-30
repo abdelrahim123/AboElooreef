8cc948570d1f1c9f2fd1d9a53a1e4d0d6c39eab0
abdelrahim123 <48460743+abdelrahim123@users.noreply.github.com>
2019-07-30 03:13:45 +0200

    lastDesign

--- js/e.js
+++ js/e.js
@@ -1,78 +1,33 @@
-/* JS Document */
-
-/******************************
-
-[Table of Contents]
-
-1. Vars and Inits
-2. Set Header
-3. Init Menu
-4. Init Home Slider
-5. Init Big Room Slider
-6. Init Gallery Slider
-7. Init Lightbox
-8. Init Magic
-
-
-******************************/
-
-$(document).ready(function()
-{
-	"use strict";
-
-	/* 
-
-	1. Vars and Inits
-
-	*/
-
-	var header = $('.header');
-	var menu = $('.menu');
-	var menuActive = false;
-	initMenu();
-/*
-	3. Init Menu
-
-	*/
-
-	function initMenu()
-	{
-		if($('.hamburger').length && $('.menu').length)
-		{
-			var hamb = $('.hamburger');
-
-			hamb.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
-		}
-	}
-
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
-
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
-    
-    $('.x').onclick(function(){
+var d1 = document.getElementById('d1');
+var d2 = document.getElementById('d2');
+var d3 = document.getElementById('d3');
+var d4 = document.getElementById('d4');
+var p1 = document.getElementById('p1');
+var p2 = document.getElementById('p2');
+var p3 = document.getElementById('p3');
+var p4 = document.getElementById('p4');
+d1.onclick=function(){
+    if(p1.style.display=="none")
+        p1.style.display="block";
+    else
+        p1.style.display="none";
         
-        closeMenu();
-        
-    });
-    
-    
-
-});
+}
+d2.onclick=function(){
+    if(p2.style.display=="none")
+        p2.style.display="block";
+    else
+        p2.style.display="none";
+}
+d3.onclick=function(){
+    if(p3.style.display=="none")
+        p3.style.display="block";
+    else
+        p3.style.display="none"
+}
+d4.onclick=function(){
+    if(p4.style.display=="none")
+        p4.style.display="block";
+    else
+        p4.style.display="none"
+}
